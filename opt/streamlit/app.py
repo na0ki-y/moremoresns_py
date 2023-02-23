72116b9b24ed8db3f6ba2d15bab6a8e7edfc1834
na0ki_mac <wehallo53.en@gmail.com>
2023-02-23 13:56:22 +0900

    ブロードキャストでURLglobal実行

--- opt/streamlit/app.py
+++ opt/streamlit/app.py
@@ -1,5 +1,24 @@
 import streamlit as st
 
+
+path_global='./reminder_setting.json'
+URL_server="http://127.0.0.1:8000"#localのfastapiサーバ
+def make_setting_file():
+    '''
+    リマインダー設定ファイルのjsonがないときに作成
+    '''
+    afte_out_put={"10s":True,"1h":False,"HH:MM":["08:00"]}
+    with  open(path_setting, "w") as f:
+            json.dump(afte_out_put, f, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
+def load_setting():
+    '''
+    リマインダー設定ファイルのjsonを読み込む
+    ignoreであるのでないときには、作成(make_setting_file())
+    '''
+    if not os.path.isfile('./reminder_setting.json'):
+        make_setting_file()
+    setting_json = json.load(open(path_setting, 'r'))
+    return setting_json
 def main():
     if 'snstext' not in st.session_state: 
 	    st.session_state.snstext = "None"
